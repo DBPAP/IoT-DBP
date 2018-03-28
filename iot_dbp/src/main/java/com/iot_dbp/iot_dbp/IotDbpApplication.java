c5b014ee62d5084c36af31a9b735a4055c0e4f30
Tito <Tito@NOTEBOOK-Gio>
2018-03-28 22:51:05 +0200

    first upload

---
+++ iot_dbp/src/main/java/com/iot_dbp/iot_dbp/IotDbpApplication.java
@@ -0,0 +1,12 @@
+package com.iot_dbp.iot_dbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDbpApplication.class, args);
+	}
+}
---
+++ iot_dbp/src/test/java/com/iot_dbp/iot_dbp/IotDbpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iot_dbp.iot_dbp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IotDbpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
