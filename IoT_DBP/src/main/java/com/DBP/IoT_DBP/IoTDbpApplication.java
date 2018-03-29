2585503a7167e707f566bd0375cc5f97bd1b0fad
Tito <Tito@NOTEBOOK-Gio>
2018-03-29 15:54:18 +0200

    First commint prova

--- IoT_DBP/src/main/java/com/DBP/IoT_DBP/IoTDbpApplication.java
+++ IoT_DBP/src/main/java/com/DBP/IoT_DBP/IoTDbpApplication.java
@@ -1,12 +1,12 @@
-package com.iot_dbp.iot_dbp;
+package com.DBP.IoT_DBP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IotDbpApplication {
+public class IoTDbpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IotDbpApplication.class, args);
+		SpringApplication.run(IoTDbpApplication.class, args);
 	}
 }
--- IoT_DBP/src/test/java/com/DBP/IoT_DBP/IoTDbpApplicationTests.java
+++ IoT_DBP/src/test/java/com/DBP/IoT_DBP/IoTDbpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iot_dbp.iot_dbp;
+package com.DBP.IoT_DBP;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IotDbpApplicationTests {
+public class IoTDbpApplicationTests {
 
 	@Test
 	public void contextLoads() {
