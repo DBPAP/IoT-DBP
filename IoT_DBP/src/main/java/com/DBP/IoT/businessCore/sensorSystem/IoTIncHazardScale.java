227e085d0081673715e3897f743c21048f10eadb
Tito <Tito@NOTEBOOK-Gio>
2018-04-07 18:11:29 +0200

    commit

--- IoT_DBP/src/main/java/com/DBP/IoT/businessCore/sensorSystem/IoTIncHazardScale.java
+++ IoT_DBP/src/main/java/com/DBP/IoT/businessCore/sensorSystem/IoTIncHazardScale.java
@@ -1,12 +1,6 @@
-package businessCore.sensorSystem;
+package com.DBP.IoT.businessCore.sensorSystem;
 
 public enum IoTIncHazardScale implements EnumScale {
-	;
-
-	public int none = 0;
-	public int low = 1;
-	public int medium = 2;
-	public int high = 3;
-	public int critical = 4;
+	NONE, LOW, MEDIUM, HIGH, CRITICAL;
 
 }
